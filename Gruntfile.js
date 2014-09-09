4fe28f555b5f04da8c888232bc3761893479b22e
Jesper Johansen <kontakt@jayj.dk>
2014-09-09 20:21:15 +0200

    Add kit grunt task and remove the Jekyll ones

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,87 +2,121 @@
 
 module.exports = function(grunt) {
 
-    "use strict";
+    'use strict';
 
+    // Load all grunt tasks
+    require( 'load-grunt-tasks' )(grunt);
+
+    // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON( "package.json" ),
+        pkg: grunt.file.readJSON( 'package.json' ),
 
-        sass: {
-            global: {
-                options: {
-                    style: "expanded"
-                },
-                files: {
-                    "css/main-unprefixed.css": "scss/main.scss"
-                }
-            }
-        },
-
-        autoprefixer: {
-            global: {
-                src: "css/main-unprefixed.css",
-                dest: "css/main.css"
-            }
-        },
-
-        shell: {
-            jekyllServe: {
-                command: "jekyll serve --baseurl="
-            },
-            jekyllBuild: {
-                command: "jekyll build"
-            }
-        },
-
+        // Watch for file changes
         watch: {
             options: {
                 livereload: false
             },
             site: {
                 files: [
-                    "index.html",
-                    "about.md",
-                    "_layouts/*.html",
-                    "_posts/*",
-                    "_projects/*",
-                    "_includes/*.html"
+                    'index.kit',
+                    'includes/*',
+                    'pages/*'
                 ],
-                tasks: [ "shell:jekyllBuild" ]
+                tasks: [ 'codekit' ]
             },
             js: {
-                files: [ "js/*.js" ],
-                tasks: [ "shell:jekyllBuild" ]
+                files: [ 'js/*.js' ],
+                tasks: [ 'jshint:js' ]
             },
             css: {
-                files: [ "scss/*.scss" ],
-                tasks: [ "sass", "autoprefixer", "shell:jekyllBuild" ]
+                files: [ 'scss/{,*/}*.scss' ],
+                tasks: [ 'sass', 'autoprefixer' ]
+            },
+            images: {
+                files: [ 'images/{,*/}*' ],
+                tasks: [ 'imagemin' ]
             },
             svgIcons: {
-                files: [ "svg/*.svg" ],
-                tasks: [ "svgstore", "shell:jekyllBuild" ]
+                files: [ 'svg/*.svg' ],
+                tasks: [ 'svgstore', 'codekit' ]
+            },
+            grunt: {
+                files: [ 'Gruntfile.js' ],
+                tasks: [ 'jshint:grunt' ]
             }
         },
 
+        // Compile .kit files to HTML
+        codekit: {
+            global: {
+                files : {
+                    'index.html' : 'index.kit'
+                }
+            },
+        },
+
+        // Compile Sass
+        sass: {
+            global: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    'css/main.css': 'scss/main.scss'
+                }
+            }
+        },
+
+        // Autoprefix the compiled CSS
+        autoprefixer: {
+            global: {
+                src: 'css/main.css',
+                dest: 'css/main.css'
+            }
+        },
+
+        // Make sure there are no obvious mistakes in the JS files
+        jshint: {
+            js: {
+                src: [ 'js/*.js' ]
+            },
+            grunt: {
+                src: [ 'Gruntfile.js' ]
+            }
+        },
+
+        // Minify images
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    dest: 'images/',
+                    src: [ '**/*.{png,jpg,gif}' ]
+                }]
+            }
+        },
+
+        // Merge SVGs from folder into a single file
         svgstore: {
             options: {
-                prefix : "shape-",
+                prefix : 'icon-',
                 cleanup: false,
                 svg: {
-                    style: "display: none;"
+                    style: 'display: none;'
                 }
             },
             default: {
                 files: {
-                    "_includes/svg-defs.svg": [ "svg/*.svg" ]
+                    '_includes/svg-defs.svg': [ 'svg/*.svg' ]
                 }
             }
-        }
+        },
 
     });
 
-  require( "load-grunt-tasks" )(grunt);
 
-  grunt.registerTask( "serve", [ "shell:jekyllServe" ] );
-  grunt.registerTask( "default", [ "sass", "autoprefixer", "svgstore", "shell:jekyllBuild", "watch" ] );
+    // Default task
+    grunt.registerTask( 'default', [ 'sass', 'autoprefixer', 'svgstore', 'codekit', 'watch' ] );
 
 };
