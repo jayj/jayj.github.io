a33cc5a9fdfc5a71e33a83ee638a450866a77a9e
Jesper Johansen <kontakt@jayj.dk>
2014-10-03 16:08:24 +0200

    Animate scroll to top when clicking on footer link

--- js/navigation.js
+++ js/navigation.js
@@ -14,6 +14,12 @@
         updateHash: true
     });
 
+    /* Scroll to top */
+    $( 'footer').find( 'a[href="#top"]' ).on( 'click', function(e) {
+        $( 'html, body' ).animate({ scrollTop: 0 }, 400 );
+        e.preventDefault();
+    });
+
 
     /*
      * Fixed navigation when the header is not longer visible.
