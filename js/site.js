2e61a5d8f2b3054da702988e4f62850bfd7da383
Jesper Johansen <kontakt@jayj.dk>
2014-10-17 23:39:02 +0200

    Toggle the navigation on click

--- js/site.js
+++ js/site.js
@@ -14,6 +14,9 @@
     nav.singlePageNav({
         offset: nav.outerHeight(),
         updateHash: true
+    /* Toggle the navigation when clicking on the toggle button */
+    $( '.toggle-nav' ).on( 'click', function() {
+        body.toggleClass( 'nav-toggled' );
     });
 
     /* Scroll to top */
