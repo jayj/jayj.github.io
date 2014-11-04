579e0edf0295459f4f70dbcebe87e6ef79a7fe1a
Jesper Johansen <kontakt@jayj.dk>
2014-11-04 22:47:10 +0100

    Set focus to the current section on the page for better accessibility when using keyboard navigation.

--- js/navigation.js
+++ js/navigation.js
@@ -16,6 +16,9 @@
         // Close the navigation
         beforeStart: function() {
             body.removeClass( 'nav-toggled' );
+        },
+        onComplete: function() {
+            $(document).trigger( 'navChangeComplete' );
         }
     });
 
@@ -31,6 +34,7 @@
         $( 'html, body' ).animate({ scrollTop: 0 }, 400 );
         e.preventDefault();
     });
+
 
     /*
      * Fixed navigation when the header is not longer visible.
@@ -50,5 +54,30 @@
     fixedMenu();
 
     _window.on( 'scroll', fixedMenu);
+
+
+    /*
+     * Set focus to the current section on the page for better accessibility
+     * when using keyboard navigation.
+     *
+     * @link http://www.nczonline.net/blog/2013/01/15/fixing-skip-to-content-links/
+     */
+    function fixSectionFocus() {
+        var element = document.getElementById(location.hash.substring(1));
+
+        if (element) {
+
+            if (!/^(?:a|select|input|button|textarea)$/i.test(element.tagName)) {
+                element.tabIndex = -1;
+            }
+
+            element.focus();
+        }
+    }
+
+    fixSectionFocus(); // run on page load
+
+    _window.on( 'navChangeComplete', fixSectionFocus);
+
 
 })( jQuery );
